910e16f00f811e8e5eed04ecff9b6323b24ccd8c
Cristóbal Montecino <cristobal.montecino@sansano.usm.cl>
2021-04-22 23:49:41 -0400

    fix test

--- test_parse.py
+++ test_parse.py
@@ -12,7 +12,7 @@ assert parse('\\def\\parse user: #1, number: #2{Hello #1 (###2)}\\parse user: {J
 assert parse('\\def\\defineDefineEat{\\def\\defineEat{\\def\\eat####1{}}\\defineEat}\\defineDefineEat\\eat hello') == 'ello'
 
 code ='\\def\\a#1{\\def\\b##1##2##3##4{###1}\\b{a}{b}{c}{d}}\\a'
-assert parse(code + '3') == 'c' and parse(code + '4') == 'd' and parse(code + '3') == 'c' and parse(code + '1') == 'a'
+assert parse(code + '3') == 'c' and parse(code + '4') == 'd' and parse(code + '2') == 'b' and parse(code + '1') == 'a'
 
 
 # let
@@ -22,5 +22,3 @@ assert parse('\\def\\a{hello world}\\let\\b\\a\\b') == 'hello world'
 # expandafter
 code = '\\def\\a#1{#1}\\def\\b#1{{hello #1}}'
 assert parse(code + '\\expandafter\\a\\b{world}') == 'hello world' and parse(code + '\\a\\b{world}') == '{hello world}'
-
-
